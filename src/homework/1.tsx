bf8619aa9adb5ec610e2ffdf7857664ce998148e
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2023-11-04 10:17:57 +0300

    init

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -1,13 +1,18 @@
-import React, { useEffect, useRef } from 'react';
+import React, { ReactElement, ReactNode, useEffect, useRef } from 'react';
 
 // Опишіть Props
-export function Observer({ children, onContentEndVisible }: Props) {
+
+type Props = {
+  onContentEndVisible:()=>void
+}
+
+export function Observer:ReactFC<Props>({ children, onContentEndVisible }) {
   // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
-  const endContentRef = useRef(null);
+  const endContentRef= useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options = {
+    const options:{rootMargin:string, threshold:number, root: Element|null} = {
       rootMargin: '0px',
       threshold: 1.0,
       root: null,
--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,5 +1,15 @@
 import React, {useReducer} from "react";
 
+type State = {
+  isRequestInProgress:boolean,
+  requestStep: "start" | "pending"  | "finished" | 'idle'
+}
+type Action ={
+  type: "START_REQUEST" | "PENDING_REQUEST"  | "FINISH_REQUEST" | 'RESET_REQUEST',
+  run:()=>void;
+}
+
+
 const initialState: State = {
   isRequestInProgress: false,
   requestStep: 'idle',
